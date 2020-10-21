0db20c78d6256ac2689b6dce33103a85b43dd8f3
unaguna <k.izumi.ysk@gmail.com>
2020-10-21 18:27:17 +0900

    ⭐リストを生成する機能を追加

--- ranj/__genlist.py
+++ ranj/__genlist.py
@@ -1,3 +1,14 @@
+import random
+import ranj.__gen as ranjg
+
+# 配列の要素の値の生成に使用するスキーマのデフォルト値。
+# items に指定がない場合に使用する。
+__default_items_schema = {
+    "type": "number",
+    "minimum": 0,
+    "maximum": 0,
+}
+
 def genlist(schema: dict) -> list:
     """スキーマに適合するリストを生成する。
 
@@ -8,5 +19,51 @@ def genlist(schema: dict) -> list:
         list: 生成されたリスト
     """
 
-    # TODO: 実装
-    return []
+    # 生成するリスト
+    result = []
+
+    # 生成する list の大きさの範囲
+    [min_items, max_items] = __get_length(schema)
+
+    # 生成する list の大きさ
+    item_count = random.randint(min_items, max_items)
+
+    # 要素のスキーマ
+    items_schema = schema.get("items", __default_items_schema)
+
+    # 要素を1つずつ生成
+    for i in range(item_count):
+        result.append(ranjg.gen(items_schema))
+
+    return result
+
+def __get_length(schema: dict) -> [int, int]:
+    """スキーマから minLength, maxLength の値を読み取る。
+
+    スキーマに指定がない場合でも、None は返さず2つの正整数値を返す。
+    一方のみが None (null) である場合、もう一方に矛盾しない整数値を代わりに返す。
+    両方が None (null) である場合、デフォルト値を返す。
+
+    Args:
+        schema (dict): array 型についての JsonSchema を表現するマップ
+
+    Returns:
+        [int, int]: 生成するリストの大きさの最小値と最大値
+    """
+
+    minItems: int = schema.get("minItems")
+    maxItems: int = schema.get("maxItems")
+
+    if minItems is None:
+        if maxItems is None:
+            minItems = 1
+        else:
+            minItems = min(1, maxItems)
+    
+    if maxItems is None:
+        if minItems is None:
+            maxItems = 5
+        else:
+            maxItems = max(5, minItems)
+    
+    return [minItems, maxItems]
--- ranj/test/test_genlist.py
+++ ranj/test/test_genlist.py
@@ -6,3 +6,25 @@ class TestGenlist(unittest.TestCase):
     def test_genlist_with_empty_schema(self):
         schema = {}
         self.assertIsInstance(genlist(schema), list)
+
+    def test_genlist_with_tight_length(self):
+        schema = {
+            "type": "array",
+            "minItems": 6,
+            "maxItems": 6,
+        }
+        generated = genlist(schema)
+        self.assertEqual(len(generated), 6)
+
+    def test_genlist_with_single_items(self):
+        schema = {
+            "type": "array",
+            "minItems": 5,
+            "items": {
+                "type": "string",
+            }
+        }
+        generated = genlist(schema)
+        for item in generated:
+            self.assertIsInstance(item, str)
+    
