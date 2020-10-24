4d775c63048bbc2fcf96390b313ea5335e755e68
unaguna <k.izumi.ysk@gmail.com>
2020-10-24 12:51:36 +0900

    ♻__init__の編成を修正

--- ranj/__init__.py
+++ ranj/__init__.py
@@ -1,19 +1,9 @@
-import ranj.__gen as __gen
-import ranj.__gennone as __gennone
-import ranj.__genbool as __genbool
-import ranj.__genint as __genint
-import ranj.__gennum as __gennum
-import ranj.__genstr as __genstr
-import ranj.__gendict as __gendict
-import ranj.__genlist as __genlist
-import ranj.__genany as __genany
-
-gen = __gen.gen
-gennone = __gennone.gennone
-genbool = __genbool.genbool
-genint = __genint.genint
-gennum = __gennum.gennum
-genstr = __genstr.genstr
-gendict = __gendict.gendict
-genlist = __genlist.genlist
-genany = __genany.genany
+from ranj.__gen import gen
+from ranj.__gennone import gennone
+from ranj.__genbool import genbool
+from ranj.__genint import genint
+from ranj.__gennum import gennum
+from ranj.__genstr import genstr
+from ranj.__gendict import gendict
+from ranj.__genlist import genlist
+from ranj.__genany import genany
