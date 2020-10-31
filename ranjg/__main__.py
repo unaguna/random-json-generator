70096d5a2b530b474129d6b6b4e1d22464829d23
unaguna <k.izumi.ysk@gmail.com>
2020-10-31 11:51:00 +0900

    ⭐モジュール実行で結果を標準出力へ出力できるよう変更

--- ranjg/__main__.py
+++ ranjg/__main__.py
@@ -1,11 +1,17 @@
+import sys
 import argparse
 from . import gen
 
 parser = argparse.ArgumentParser(description="Generate json file randomly according to json schema.")
 
 parser.add_argument("schema_file_path", help="Path of json schema file. This file is used as base schema.")
-parser.add_argument("--jsonoutput", "-j", required=True, help="Path to which json file is written.")
+parser.add_argument("--jsonoutput", "-j", help="Path to which json file is written.")
 
 args = parser.parse_args()
 
-gen(schema_file=args.schema_file_path, output_file=args.jsonoutput)
+# 出力先を決定
+output_file, output_fp = (args.jsonoutput, None) \
+    if args.jsonoutput is not None \
+    else (None, sys.stdout)
+
+gen(schema_file=args.schema_file_path, output_file=output_file, output_fp=output_fp)
