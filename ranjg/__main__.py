00b0046f0ed972eb330261cebb8755a166fd2794
unaguna <k.izumi.ysk@gmail.com>
2020-10-31 11:30:43 +0900

    💄将来オプションファイル指定に使いそうな -o をとっておく

--- ranjg/__main__.py
+++ ranjg/__main__.py
@@ -4,8 +4,8 @@ from . import gen
 parser = argparse.ArgumentParser(description="Generate json file randomly according to json schema.")
 
 parser.add_argument("schema_file_path", help="Path of json schema file. This file is used as base schema.")
-parser.add_argument("--outputpath", "-o", required=True, help="Path to which json file is written.")
+parser.add_argument("--jsonoutput", "-j", required=True, help="Path to which json file is written.")
 
 args = parser.parse_args()
 
-gen(schema_file=args.schema_file_path, output_file=args.outputpath)
+gen(schema_file=args.schema_file_path, output_file=args.jsonoutput)
