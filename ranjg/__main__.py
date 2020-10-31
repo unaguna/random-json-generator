8281b26747f20bfaaccf22d9a81f083c9f9b6132
unaguna <k.izumi.ysk@gmail.com>
2020-10-31 11:53:36 +0900

    ♻引数パースを関数として分離

--- ranjg/__main__.py
+++ ranjg/__main__.py
@@ -2,12 +2,15 @@ import sys
 import argparse
 from . import gen
 
-parser = argparse.ArgumentParser(description="Generate json file randomly according to json schema.")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate json file randomly according to json schema.")
 
-parser.add_argument("schema_file_path", help="Path of json schema file. This file is used as base schema.")
-parser.add_argument("--jsonoutput", "-j", help="Path to which json file is written.")
+    parser.add_argument("schema_file_path", help="Path of json schema file. This file is used as base schema.")
+    parser.add_argument("--jsonoutput", "-j", help="Path to which json file is written.")
 
-args = parser.parse_args()
+    return parser.parse_args()
+
+args = parse_args()
 
 # 出力先を決定
 output_file, output_fp = (args.jsonoutput, None) \
