9df9e7f18f8a9bda4f2d0470a87dc6c62284c20e
unaguna <k.izumi.ysk@gmail.com>
2020-11-07 19:02:22 +0900

    ♻InvalidSchemaErrorの一部をValidationErrorWrapperへ分離

--- ranjg/error/__invalid_schema_error.py
+++ ranjg/error/__invalid_schema_error.py
@@ -3,11 +3,12 @@
 スキーマ不正エラークラス ``InvalidSchemaError`` を__init__へ提供するモジュール。
 """
 import os
-from typing import List, Iterable, Union
+from typing import List, Iterable, Union, Iterator
+
 from jsonschema import ValidationError
 
 
-__INDENT_UNIT_LENGTH = 4
+_INDENT_UNIT_LENGTH = 4
 
 
 def __path_part_to_str(path_part: Union[str, int]) -> str:
@@ -24,7 +25,7 @@ def __path_part_to_str(path_part: Union[str, int]) -> str:
         else f"[\"{path_part}\"]"
 
 
-def __path_to_str(path: Iterable[Union[str, int]]) -> str:
+def _path_to_str(path: Iterable[Union[str, int]]) -> str:
     """JSONパスを文字列へ変換
 
     Args:
@@ -36,42 +37,11 @@ def __path_to_str(path: Iterable[Union[str, int]]) -> str:
     return "".join(map(__path_part_to_str, path))
 
 
-def _make_message_by_error(error: ValidationError, indent: int = __INDENT_UNIT_LENGTH) -> List[str]:
-    """バリデーションエラー毎のエラーメッセージを生成する。
-
-    Args:
-        error: バリデーションエラー
-        indent: 各行の先頭につけるスペースの数
-
-    Returns:
-        メッセージの各行からなるリスト
-    """
-
-    def context_to_message_line(e: ValidationError):
-        """エラーの文脈毎のエラーメッセージを生成する。
-
-        Args:
-            e: バリデーションエラー
-
-        Returns:
-            指定したバリデーションエラーが発生するに至る原因となったバリデーションエラーのエラーメッセージの各行からなるリスト。
-        """
-        return _make_message_by_error(e, indent + 2 * __INDENT_UNIT_LENGTH)
-
-    path_str = __path_to_str(error.absolute_path)
-    context_line_list = sum(map(context_to_message_line, error.context), [])
-
-    return [
-               f"{indent * ' '}On instance{path_str}:",
-               f"{(indent + __INDENT_UNIT_LENGTH) * ' '}{error.message}",
-           ] + context_line_list
-
-
 class InvalidSchemaError(Exception):
     """スキーマ不正エラークラス
 
     Attributes:
-        __validation_error_list (List[ValidationError]):
+        __cause_list (List[ValidationErrorWrapper]):
             このエラーの原因となったバリデーションエラーのリスト
     """
 
@@ -83,8 +53,71 @@ class InvalidSchemaError(Exception):
             validation_error_list:
                 このエラーの原因となったバリデーションエラーのリスト
         """
-        self.__validation_error_list = validation_error_list
+        self.__cause_list = map(lambda e: ValidationErrorWrapper(e), validation_error_list)
 
-        message_line_list = sum(map(_make_message_by_error, validation_error_list), [])
+        message_line_list = self._make_message_line_list()
 
         super().__init__(os.linesep + os.linesep.join(message_line_list))
+
+    def _make_message_line_list(self) -> List[str]:
+        """エラーメッセージ作成
+
+        Returns:
+            メッセージの各行からなるリスト
+        """
+        return sum(map(lambda c: c.make_message(), self.__cause_list), [])
+
+
+class ValidationErrorWrapper:
+    """バリデーションエラークラスのラッパー
+    """
+
+    @property
+    def base_error(self) -> ValidationError:
+        """バリデーションエラー
+        """
+        return self.__base_error
+
+    @property
+    def context(self) -> Iterator:
+        """エラー文脈
+        """
+        return map(lambda e: ValidationErrorWrapper(e), self.base_error.context)
+
+    def __init__(self, base_error: ValidationError):
+        self.__base_error = base_error
+
+    def make_message(self, indent: int = _INDENT_UNIT_LENGTH) -> List[str]:
+        """バリデーションエラーのエラーメッセージを生成する。
+
+        Args:
+            indent: 各行の先頭につけるスペースの数
+
+        Returns:
+            メッセージの各行からなるリスト
+        """
+        path_str = _path_to_str(self.base_error.absolute_path)
+        context_line_list = self._make_message_of_context(indent)
+
+        return [
+                   f"{indent * ' '}On instance{path_str}:",
+                   f"{(indent + _INDENT_UNIT_LENGTH) * ' '}{self.base_error.message}",
+               ] + context_line_list
+
+    def _make_message_of_context(self, indent: int):
+        """エラー文脈のエラーメッセージを作成する。
+
+        バリデーションエラーの原因となったすべてのバリデーションエラーからメッセージを作成する。
+
+        Args:
+            indent: 各行の先頭につけるスペースの数
+
+        Returns:
+            メッセージの各行からなるリスト
+        """
+
+        def context_to_message_line(e: ValidationErrorWrapper):
+            return e.make_message(indent + 2 * _INDENT_UNIT_LENGTH)
+
+        return sum(map(context_to_message_line, self.context), [])
+
