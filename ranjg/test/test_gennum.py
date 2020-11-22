07533dfde4f50e78e19194835213d9e96fae07e6
unaguna <k.izumi.ysk@gmail.com>
2020-11-22 18:04:06 +0900

    ✅数値範囲決定処理のテストを追加

--- ranjg/test/test_gennum.py
+++ ranjg/test/test_gennum.py
@@ -1,6 +1,8 @@
+import itertools
 import unittest
 import jsonschema
 from ranjg import gennum
+from ..__gennum import NumberRange
 from ranjg.error import SchemaConflictError
 
 
@@ -292,3 +294,151 @@ class TestGennum(unittest.TestCase):
                     "exclusiveMaximum": exclusive_maximum,
                 }
                 self.assertRaises(SchemaConflictError, lambda: gennum(schema))
+
+
+class TestGennumRangeFromSchema(unittest.TestCase):
+    """Test class of ``NumberRange.from_schema``
+
+    Test ``ranjg.__gennum.NumberRange.from_schema``
+    """
+
+    def test_make_range_with_empty_schema(self):
+        """ Normalized System Test
+
+        When ``schema`` is empty, both of ``minimum`` and ``maximum`` is not specified. If ``minimum`` is not specified,
+        ``exclusive_minimum`` is False. ``maximum`` and ``exclusive_maximum`` are same to them.
+
+        assert that:
+            When the schema is empty, ``minimum`` and ``maximum`` is None and ``exclusive_minimum`` and
+            ``exclusive_maximum`` are False.
+        """
+        schema = {}
+        number_range = NumberRange.from_schema(schema)
+        self.assertIsNone(number_range.minimum)
+        self.assertIsNone(number_range.maximum)
+        self.assertFalse(number_range.exclusive_minimum)
+        self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_min(self):
+        """ Normalized System Test
+
+        When ``schema.minimum`` is specified and ``schema.exclusiveMinimum`` is not specified, the ``minimum`` of result
+        equals to ``schema.minimum`` and ``exclusive_minimum`` is False.
+
+        assert that:
+            When only ``schema.minimum`` is specified, the ``minimum`` of result equals to ``schema.minimum`` and
+            ``exclusive_minimum`` is False.
+        """
+        minimum_list = (-3, 0.0, 2.2)
+
+        for minimum in minimum_list:
+            with self.subTest(minimum=minimum):
+                schema = {
+                    "minimum": minimum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertEqual(number_range.minimum, minimum)
+                self.assertIsNone(number_range.maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_exMin_bool(self):
+        """ Normalized System Test
+
+        When ``schema.exclusiveMinimum`` is specified with boolean value and ``schema.minimum`` is not specified, they
+        are ignored.
+
+        assert that:
+            When ``schema.exclusiveMinimum`` is boolean value and ``schema.minimum`` is not specified, ``minimum`` and
+            ``maximum`` is None and ``exclusive_minimum`` and ``exclusive_maximum`` are False.
+        """
+        exclusive_minimum_list = (True, False)
+
+        for exclusive_minimum in exclusive_minimum_list:
+            with self.subTest(ex_min=exclusive_minimum):
+                schema = {
+                    "exclusiveMinimum": exclusive_minimum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertIsNone(number_range.minimum)
+                self.assertIsNone(number_range.maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_min_exMin_bool(self):
+        """ Normalized System Test
+
+        When ``schema.minimum`` and ``schema.exclusiveMinimum`` are specified, the ``minimum`` of result
+        equals to ``schema.minimum`` and ``exclusive_minimum`` equals to ``schema.exclusiveMinimum``.
+
+        assert that:
+            When ``schema.minimum`` and ``schema.exclusiveMinimum`` is specified, the ``minimum`` of result equals to
+            ``schema.minimum`` and the ``exclusive_minimum`` equals to ``schema.exclusiveMinimum``.
+        """
+        minimum_list = (-3, 0.0, 2.2)
+        exclusive_minimum_list = (True, False)
+
+        for minimum, exclusive_minimum in itertools.product(minimum_list, exclusive_minimum_list):
+            with self.subTest(minimum=minimum, ex_min=exclusive_minimum):
+                schema = {
+                    "minimum": minimum,
+                    "exclusiveMinimum": exclusive_minimum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertEqual(number_range.minimum, minimum)
+                self.assertIsNone(number_range.maximum)
+                self.assertEqual(number_range.exclusive_minimum, exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_exMin_number(self):
+        """ Normalized System Test
+
+        When ``schema.minimum`` is not specified and ``schema.exclusiveMinimum`` is specified with number, the
+        ``minimum`` of result equals to ``schema.exclusiveMinimum`` and ``exclusive_minimum`` is True.
+
+        assert that:
+            When only ``schema.exclusiveMinimum`` is specified and it is number, the ``minimum`` of result equals to
+            ``schema.exclusiveMinimum`` and ``exclusive_minimum`` is True.
+        """
+        minimum_list = (-3, 0.0, 2.2)
+
+        for exclusive_minimum in minimum_list:
+            with self.subTest(ex_min=exclusive_minimum):
+                schema = {
+                    "exclusiveMinimum": exclusive_minimum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertEqual(number_range.minimum, exclusive_minimum)
+                self.assertIsNone(number_range.maximum)
+                self.assertTrue(number_range.exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_min_exMin_number(self):
+        """ Normalized System Test
+
+        When ``schema.minimum`` and ``schema.exclusiveMinimum`` are specified with number, the stricter one applies.
+
+        assert that:
+            When only ``schema.minimum > schema.exclusiveMinimum``, the ``minimum`` of result equals to
+            ``schema.minimum`` and ``exclusive_minimum`` is False.
+            When only ``schema.minimum <= schema.exclusiveMinimum``, the ``minimum`` of result equals to
+            ``schema.exclusiveMinimum`` and ``exclusive_minimum`` is True.
+        """
+        param_list = ((-2.0, -2.0, -2.0, True),
+                      (-2.0, -1.9, -1.9, True),
+                      (-1.9, -2.0, -1.9, False),
+                      (0, 0, 0, True),
+                      (0, .1, .1, True),
+                      (.1, 0, .1, False))
+
+        for minimum, exclusive_minimum, expected_minimum, expected_exclusive_minimum in param_list:
+            with self.subTest(minimum=minimum, ex_min=exclusive_minimum):
+                schema = {
+                    "minimum": minimum,
+                    "exclusiveMinimum": exclusive_minimum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertEqual(number_range.minimum, expected_minimum)
+                self.assertIsNone(number_range.maximum)
+                self.assertEqual(number_range.exclusive_minimum, expected_exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
