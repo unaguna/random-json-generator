8f982769f17167ba9b6a656c3cc6a72887a1d886
unaguna <k.izumi.ysk@gmail.com>
2020-11-22 18:07:07 +0900

    ✅数値範囲決定処理のテストを追加

--- ranjg/test/test_gennum.py
+++ ranjg/test/test_gennum.py
@@ -442,3 +442,127 @@ class TestGennumRangeFromSchema(unittest.TestCase):
                 self.assertIsNone(number_range.maximum)
                 self.assertEqual(number_range.exclusive_minimum, expected_exclusive_minimum)
                 self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_max(self):
+        """ Normalized System Test
+
+        When ``schema.maximum`` is specified and ``schema.exclusiveMaximum`` is not specified, the ``maximum`` of result
+        equals to ``schema.maximum`` and ``exclusive_maximum`` is False.
+
+        assert that:
+            When only ``schema.maximum`` is specified, the ``maximum`` of result equals to ``schema.maximum`` and
+            ``exclusive_maximum`` is False.
+        """
+        maximum_list = (-3, 0.0, 2.2)
+
+        for maximum in maximum_list:
+            with self.subTest(maximum=maximum):
+                schema = {
+                    "maximum": maximum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertIsNone(number_range.minimum)
+                self.assertEqual(number_range.maximum, maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_exMax_bool(self):
+        """ Normalized System Test
+
+        When ``schema.exclusiveMaximum`` is specified with boolean value and ``schema.maximum`` is not specified, they
+        are ignored.
+
+        assert that:
+            When ``schema.exclusiveMaximum`` is boolean value and ``schema.maximum`` is not specified, ``maximum`` and
+            ``maximum`` is None and ``exclusive_maximum`` and ``exclusive_maximum`` are False.
+        """
+        exclusive_maximum_list = (True, False)
+
+        for exclusive_maximum in exclusive_maximum_list:
+            with self.subTest(ex_max=exclusive_maximum):
+                schema = {
+                    "exclusiveMaximum": exclusive_maximum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertIsNone(number_range.minimum)
+                self.assertIsNone(number_range.maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertFalse(number_range.exclusive_maximum)
+
+    def test_make_range_with_max_exMax_bool(self):
+        """ Normalized System Test
+
+        When ``schema.maximum`` and ``schema.exclusiveMaximum`` are specified, the ``maximum`` of result
+        equals to ``schema.maximum`` and ``exclusive_maximum`` equals to ``schema.exclusiveMaximum``.
+
+        assert that:
+            When ``schema.maximum`` and ``schema.exclusiveMaximum`` is specified, the ``maximum`` of result equals to
+            ``schema.maximum`` and the ``exclusive_maximum`` equals to ``schema.exclusiveMaximum``.
+        """
+        maximum_list = (-3, 0.0, 2.2)
+        exclusive_maximum_list = (True, False)
+
+        for maximum, exclusive_maximum in itertools.product(maximum_list, exclusive_maximum_list):
+            with self.subTest(maximum=maximum, ex_max=exclusive_maximum):
+                schema = {
+                    "maximum": maximum,
+                    "exclusiveMaximum": exclusive_maximum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertIsNone(number_range.minimum)
+                self.assertEqual(number_range.maximum, maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertEqual(number_range.exclusive_maximum, exclusive_maximum)
+
+    def test_make_range_with_exMax_number(self):
+        """ Normalized System Test
+
+        When ``schema.maximum`` is not specified and ``schema.exclusiveMaximum`` is specified with number, the
+        ``maximum`` of result equals to ``schema.exclusiveMaximum`` and ``exclusive_maximum`` is True.
+
+        assert that:
+            When only ``schema.exclusiveMaximum`` is specified and it is number, the ``maximum`` of result equals to
+            ``schema.exclusiveMaximum`` and ``exclusive_maximum`` is True.
+        """
+        maximum_list = (-3, 0.0, 2.2)
+
+        for exclusive_maximum in maximum_list:
+            with self.subTest(ex_max=exclusive_maximum):
+                schema = {
+                    "exclusiveMaximum": exclusive_maximum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertIsNone(number_range.minimum)
+                self.assertEqual(number_range.maximum, exclusive_maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertTrue(number_range.exclusive_maximum)
+
+    def test_make_range_with_max_exMax_number(self):
+        """ Normalized System Test
+
+        When ``schema.maximum`` and ``schema.exclusiveMaximum`` are specified with number, the stricter one applies.
+
+        assert that:
+            When only ``schema.maximum > schema.exclusiveMaximum``, the ``maximum`` of result equals to
+            ``schema.maximum`` and ``exclusive_maximum`` is False.
+            When only ``schema.maximum <= schema.exclusiveMaximum``, the ``maximum`` of result equals to
+            ``schema.exclusiveMaximum`` and ``exclusive_maximum`` is True.
+        """
+        param_list = ((-2.0, -2.0, -2.0, True),
+                      (-2.0, -2.1, -2.1, True),
+                      (-2.1, -2.0, -2.1, False),
+                      (0, 0, 0, True),
+                      (0, -.1, -.1, True),
+                      (-.1, 0, -.1, False))
+
+        for maximum, exclusive_maximum, expected_maximum, expected_exclusive_maximum in param_list:
+            with self.subTest(maximum=maximum, ex_max=exclusive_maximum):
+                schema = {
+                    "maximum": maximum,
+                    "exclusiveMaximum": exclusive_maximum,
+                }
+                number_range = NumberRange.from_schema(schema)
+                self.assertIsNone(number_range.minimum)
+                self.assertEqual(number_range.maximum, expected_maximum)
+                self.assertFalse(number_range.exclusive_minimum)
+                self.assertEqual(number_range.exclusive_maximum, expected_exclusive_maximum)
