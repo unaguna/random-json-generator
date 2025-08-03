16a1cc90452dddfbefbae60b8ca4d6474a5c0c83
K-izumi <k.izumi.ysk@gmail.com>
2025-08-03 11:49:14 +0900

    🚀バージョン番号を更新

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def load_readme() -> str:
 
 setup(
     name='ranjg',
-    version='0.5.0.1',
+    version='0.5.1.0',
     project_urls={
         "Bug Tracker": 'https://github.com/unaguna/random-json-generator/issues',
         "Documentation": 'https://unaguna.github.io/random-json-generator/',
